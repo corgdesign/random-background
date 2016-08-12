b9198e1c53374c8475e2f396d408f5f5e29236bd
Corg Design <corgdesign@gmail.com>
2016-08-12 20:49:01 +0100

    Create randomBackground.js
    Initial commit

---
+++ randomBackground.js
@@ -0,0 +1,13 @@
+function randomBackground(element) {
+		
+		var rgb1, rgb2, rgb3;
+		
+		rgb1 = Math.floor( (Math.random() * 255) );
+		rgb2 = Math.floor( (Math.random() * 255) );
+		rgb3 = Math.floor( (Math.random() * 255) );
+		
+		var rgba = "rgba(" + rgb1 + "," + rgb2 + "," + rgb3 + ", 0.2)";
+		
+		$(element).css('background-color', rgba);
+	
+	};

12c312ada6bc4c49316fae098eaaa08f0493baaf
Corg Design <corgdesign@gmail.com>
2016-08-12 21:08:55 +0100

    Update randomBackground.jquery.js

--- randomBackground.jquery.js
+++ randomBackground.jquery.js
@@ -1,13 +1,13 @@
 function randomBackground(element) {
 		
-		var rgb1, rgb2, rgb3;
+	var rgb1, rgb2, rgb3;
 		
-		rgb1 = Math.floor( (Math.random() * 255) );
-		rgb2 = Math.floor( (Math.random() * 255) );
-		rgb3 = Math.floor( (Math.random() * 255) );
+	rgb1 = Math.floor( (Math.random() * 255) );
+	rgb2 = Math.floor( (Math.random() * 255) );
+	rgb3 = Math.floor( (Math.random() * 255) );
 		
-		var rgba = "rgba(" + rgb1 + "," + rgb2 + "," + rgb3 + ", 0.2)";
+	var rgba = "rgba(" + rgb1 + "," + rgb2 + "," + rgb3 + ", 0.2)";
 		
-		$(element).css('background-color', rgba);
+	$(element).css('background-color', rgba);
 	
-	};
+};

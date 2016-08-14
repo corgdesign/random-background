633f64d218e01b9305952d77b04bd27872a159ab
Corg Design <corgdesign@gmail.com>
2016-08-14 10:05:06 +0100

    Update randomBackground.jquery.js

--- randomBackground.jquery.js
+++ randomBackground.jquery.js
@@ -1,3 +1,7 @@
+//  randomBackground.jquery.js
+//  Author: Martin Stewart, Corg Design - http://corgdesign.github.io
+//  License: MIT
+
 (function( $ ) {
 
 	$.fn.randomBackground = function (alpha) {
